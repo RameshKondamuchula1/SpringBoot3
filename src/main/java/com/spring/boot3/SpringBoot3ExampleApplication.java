f9f56265f29a55f5b57490aada65bb0312c98bd5
RameshKondamuchula1 <ramesh.kondamuchula@gmail.com>
2024-10-24 08:19:24 +0530

    Initial commit dev

---
+++ src/main/java/com/spring/boot3/SpringBoot3ExampleApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3ExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3ExampleApplication.class, args);
+	}
+
+}
